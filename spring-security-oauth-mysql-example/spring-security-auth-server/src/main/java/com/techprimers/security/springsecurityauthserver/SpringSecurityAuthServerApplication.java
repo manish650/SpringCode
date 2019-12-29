7132f37b41b50b9dc4143939287bc39ad159ee1f
manis <manis@manishLaptop>
2019-12-29 22:14:54 +0530

    added encoded

--- spring-security-oauth-mysql-example/spring-security-auth-server/src/main/java/com/techprimers/security/springsecurityauthserver/SpringSecurityAuthServerApplication.java
+++ spring-security-oauth-mysql-example/spring-security-auth-server/src/main/java/com/techprimers/security/springsecurityauthserver/SpringSecurityAuthServerApplication.java
@@ -4,21 +4,26 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.techprimers.security.springsecurityauthserver.model.Role;
 import com.techprimers.security.springsecurityauthserver.model.Users;
 import com.techprimers.security.springsecurityauthserver.repository.UsersRepository;
-
-
 
 @SpringBootApplication
 public class SpringSecurityAuthServerApplication implements CommandLineRunner {
 
 	@Autowired
 	private UsersRepository repository;
+	@Autowired
+	@Qualifier("bcryptEncoder")
+	private PasswordEncoder encoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityAuthServerApplication.class, args);
@@ -31,16 +36,21 @@ public class SpringSecurityAuthServerApplication implements CommandLineRunner {
 		role.setRole("USER");
 		Set<Role> roles = new HashSet<>();
 		roles.add(role);
- 		Users users = new Users(1,"manish@gmail.com","manish","manish","mishra",1,roles);
- 		
- 		Role role1 = new Role();
+		Users users = new Users(1, "manish@gmail.com", "manish", encoder.encode("manish"), "mishra", 1, roles);
+
+		Role role1 = new Role();
 		role.setRoleId(2);
 		role.setRole("ADMIN");
 		Set<Role> rolesNew = new HashSet<>();
 		rolesNew.add(role1);
- 		Users users2 = new Users(2,"manish@gmail.com","man","man","mishra",1,rolesNew);
- 		
- 		repository.save(users);
- 		repository.save(users2);
+		Users users2 = new Users(2, "manish@gmail.com", "man", encoder.encode("man"), "mishra", 1, rolesNew);
+
+		repository.save(users);
+		repository.save(users2);
+	}
+	
+	@Bean("bcryptEncoder")
+	public PasswordEncoder getEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
--- spring-security-oauth-mysql-example/spring-security-auth-server/src/main/java/com/techprimers/security/springsecurityauthserver/config/ResourceServerConfig.java
+++ spring-security-oauth-mysql-example/spring-security-auth-server/src/main/java/com/techprimers/security/springsecurityauthserver/config/ResourceServerConfig.java
@@ -1,43 +1,67 @@
 package com.techprimers.security.springsecurityauthserver.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @EnableResourceServer
 @Configuration
 public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	@Autowired
+	private UserDetailsService customUserDetailsService;
+	@Autowired
+	@Qualifier("bcryptEncoder")
+	private PasswordEncoder passwordEncoder;
+	
+	@Autowired
+	@Qualifier("encode")
+	private PasswordEncoder encode;
 
-    @Autowired
-    private AuthenticationManager authenticationManager;
-    @Autowired
-    private UserDetailsService customUserDetailsService;
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+		http.requestMatchers().antMatchers("/login", "/oauth/authorize").and().authorizeRequests().anyRequest()
+				.authenticated().and().formLogin().permitAll();
+	}
 
-        http.requestMatchers()
-                .antMatchers("/login", "/oauth/authorize")
-                .and()
-                .authorizeRequests()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .formLogin()
-                .permitAll();
-    }
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
+		auth.parentAuthenticationManager(authenticationManager).userDetailsService(customUserDetailsService)
+				.passwordEncoder(encode);
+	}
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	@Bean("encode")
+	public PasswordEncoder getPassEncoder() {
+		return new PasswordEncoder() {
+			@Override
+			public boolean matches(CharSequence rawPassword, String encodedPassword) {
+				if (passwordEncoder.matches(rawPassword, encodedPassword)) {
+					return true;
+				} else {
+					return false;
+				}
+			}
 
-        auth.parentAuthenticationManager(authenticationManager)
-                .userDetailsService(customUserDetailsService);
-    }
+			@Override
+			public String encode(CharSequence rawPassword) {
+				return passwordEncoder.encode(rawPassword);
+			}
+		};
+	}
+	
+	
 }
